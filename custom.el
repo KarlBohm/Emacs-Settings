28a3da28b4f7052dd07f263ee5868d02cfa0f33d
KarlBohm <thisisnotanameagain@outlook.com>
2023-02-18 02:35:27 +0800

    Change treemacs font

--- custom.el
+++ custom.el
@@ -7,10 +7,11 @@
    '("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))
  '(delete-selection-mode nil)
  '(package-selected-packages
-   '(magit counsel-projectile projectile dashboard proof-general ligature mwim amx counsel haskell-mode doom-modeline spaceline-all-the-icons ace-window undo-tree dracula-theme use-package)))
+   '(treemacs-projectile treemacs magit counsel-projectile projectile dashboard proof-general ligature mwim amx counsel haskell-mode doom-modeline spaceline-all-the-icons ace-window undo-tree dracula-theme use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(font-lock-comment-face ((t (:slant italic)))))
+ '(font-lock-comment-face ((t (:slant italic))))
+ '(variable-pitch ((t (:height 132 :family "Noto Serif")))))
--- lisp/init-prog.el
+++ lisp/init-prog.el
@@ -23,6 +23,26 @@
 (use-package magit
   :ensure t)
 
+(use-package treemacs
+  :ensure t
+  :defer t
+  :config
+  (treemacs-tag-follow-mode)
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("C-x t B"   . treemacs-bookmark)
+        ;; ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag))
+  (:map treemacs-mode-map
+	("/" . treemacs-advanced-helpful-hydra)))
+
+(use-package treemacs-projectile
+  :ensure t
+  :after (treemacs projectile))
+
 (provide 'init-prog)
 
 ;;; init-programming.el ends here
